bb6e86111ef7f8b5d4a2cd2ac093a16436db93d7
CaioPaulinoCosta <caiopaulinocosta@hotmail.com>
2023-05-20 04:25:14 -0300

    removendo o arquivo backbtn.html

--- edit.php
+++ edit.php
@@ -2,7 +2,6 @@
   include_once("templates/header.php");
 ?>
   <div class="container">
-    <?php include_once("templates/backbtn.html"); ?>
     <h1 id="main-title">Editar contato</h1>
     <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
       <input type="hidden" name="type" value="edit">
